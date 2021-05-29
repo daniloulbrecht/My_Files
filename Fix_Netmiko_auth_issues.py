7b0e02edc440e03c42d85917941282461181b7e0
daniloulbrecht <daniloulbrecht@hotmail.com>
2021-05-29 10:23:29 -0300

    Alterado função de workaround try_netmiko_three_times do script de exemplo Fix_Netmiko_auth_issues.py

--- Fix_Netmiko_auth_issues.py
+++ Fix_Netmiko_auth_issues.py
@@ -1,34 +1,37 @@
 """
+28/05/2021
+Danilo Nogueira Ulbrecht
 Em alguns ambientes a autenticação via TACACS pode falhar e isso causa
 a quebra do nosso script.
-Abaixo um exemplo para forçar 3 tentativas seguidas de acesso SSH.
+Abaixo um exemplo para forçar 5 tentativas seguidas de acesso SSH.
 """
 from netmiko import Netmiko
 
-def try_netmiko_three_times(fdevice):
+
+def try_netmiko_three_times(fdevice, ntimes):
     contador = 1
-    while contador < 4:
+    while contador < ntimes + 1:
         try:
             fsshconnection = Netmiko(**fdevice)
-            contador = 5
+            contador = ntimes + 2
         except:
-            print("Tentativa de conexão {} falhou, tentando conectar novamente ..."\
-            .format(contador))
+            print(
+                f"Tentativa de conexão {contador} falhou, tentando"
+                + " conectar novamente ...")
             contador += 1
-
-    if contador == 4:
-        raise Exception("Numero máximo de tentativas de conexão excedidos, verifique se não há problemas de autenticação.")
-
+    if contador == ntimes + 1:
+        raise RuntimeError("Numero máximo de tentativas de conexão excedidos")
     return fsshconnection
 
-device = {
-    'host': "192.168.255.31",
-    'username': "admin",
-    'password': "123456",
-    'device_type': "cisco_ios"
-    }
 
-sshconnection = try_netmiko_three_times(device)
-output = sshconnection.send_command('sh version')
+device = {
+    "host": "192.168.255.31",
+    "username": "admin",
+    "password": "123456",
+    "device_type": "cisco_ios",
+}
+
+sshconnection = try_netmiko_three_times(device, 5)
+output = sshconnection.send_command("sh version")
 print(output)
 sshconnection.disconnect()
