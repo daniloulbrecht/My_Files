56dc90d714c03dbe21a66ffb400cd56310ae13b1
daniloulbrecht <daniloulbrecht@hotmail.com>
2021-07-04 10:58:26 -0300

    modulo para autenticação e criação de sessão API em Cisco ACI

--- aci_login.py
+++ aci_login.py
@@ -13,22 +13,34 @@ def aci_auth_session(fapicip, fuser, fpassword, fcontentype='json'):
     """
     fsession_api = requests.session()
     fpayload = {"aaaUser" :{"attributes": {"name": fuser, "pwd": fpassword}}}
-    floginresponse = fsession_api.post(url="https://"+fapicip+"/api/aaaLogin.json", data=json.dumps(fpayload) ,verify=False).json()
-    ftoken = floginresponse['imdata'][0]['aaaLogin']['attributes']['token']
-    if fcontentype == 'xml':
-        fsession_api.headers.update({'APIC-cookie': ftoken, "Content-Type": "application/xml"})
+    floginresponse = fsession_api.post(url="https://"+fapicip+"/api/aaaLogin.json", data=json.dumps(fpayload), verify=False)
+    if floginresponse.status_code != 200:
+        print(json.dumps(response.json(), indent = 4))
+        exit(1)
     else:
-        fsession_api.headers.update({'APIC-cookie': ftoken, "Content-Type": "application/json"})
-    return fsession_api
+        print("Login efetuado com sucesso")
+        floginresponse = floginresponse.json()
+        ftoken = floginresponse['imdata'][0]['aaaLogin']['attributes']['token']
+        if fcontentype == 'xml':
+            fsession_api.headers.update({'APIC-cookie': ftoken, 'Content-Type': 'application/xml'})
+        else:
+            fsession_api.headers.update({'APIC-cookie': ftoken, 'Content-Type': 'application/json'})
+        return fsession_api
 
 
-#def aci_auth_logout()
-#Em construcao
+def aci_auth_logout(fapic_session, fip):
+    fresponse = fapic_session.post(url= "https://"+fip+"/api/aaaLogout.json", verify=False)
+    if fresponse.status_code != 200:
+        print(json.dumps(response.json(), indent = 4))
+    else:
+        print("Logout efetuado com sucesso")
+
 
 if __name__ == "__main__":
     user = 'admin'
     password = '12345678'
     ip = '192.168.66.3'
     apic_session = aci_auth_session(ip, user, password)
-    bdsdict = apic_session.get(url = 'https://'+ip+'/api/node/mo/uni/tn-DCLessons.json?query-target=children&target-subtree-class=fvBD').json()
+    bdsdict = apic_session.get(url = 'https://'+ip+'/api/node/mo/uni/tn-DCLessons.json?query-target=children&target-subtree-class=fvBD', verify=False).json()
     print(json.dumps(bdsdict, indent = 4))
+    aci_auth_logout(apic_session, ip)
