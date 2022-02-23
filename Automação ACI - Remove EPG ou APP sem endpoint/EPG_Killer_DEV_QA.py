8e4ff05cbd8e9e64bc47b1a6f4ee53e422a80c93
daniloulbrecht <daniloulbrecht@hotmail.com>
2022-02-22 23:14:51 -0300

    Nova automacao remove epgs e app sem endpoints

---
+++ Automação ACI - Remove EPG ou APP sem endpoint/EPG_Killer_DEV_QA.py
@@ -0,0 +1,179 @@
+import requests
+import json
+from re import sub
+from re import findall
+import warnings
+from datetime import datetime
+from sys import argv
+from exception_list_DEV_QA import exception_list_DEV_QA as exception_list
+warnings.filterwarnings('ignore', message='Unverified HTTPS request')
+
+"""
+Script remove o que nao teve endpoint por duas vezes consecutivas
+checando o arquivo ultima_consulta.txt
+Recomenda-se agendar a execucao para X dias de forma periodica
+Kubernetes removemos o application profile referente ao namespace
+Demais removemos o EPG diretamente
+A cada remocao e feito backup do fabric com data
+A cada remocao e feito backup dos EPGs ou application profiles removidos com data
+EPGs ou application profiles que nao desejamos remover mesmo sem enpoints
+colocar na lista exception_list_DEV_QA.py
+"""
+
+def aci_auth_session(fapicip, fuser, fpassword, fcontentype='json'):
+    """
+    Funcao para criar um sessao API no ACI que ja contem o token
+    bem como o content-type das operacoes posteriores (post, put),
+    dessa forma nao e necessario repetir esses parametros nas chamadas
+    apenas chamar url e payload (body).
+    """
+    fsession_api = requests.session()
+    fpayload = {"aaaUser" :{"attributes": {"name": fuser, "pwd": fpassword}}}
+    floginresponse = fsession_api.post(url="https://"+fapicip+"/api/aaaLogin.json",
+    data=json.dumps(fpayload), verify=False)
+    if floginresponse.status_code != 200:
+        print(json.dumps(response.json(), indent = 4))
+        exit(1)
+    else:
+        print("Login efetuado com sucesso")
+        floginresponse = floginresponse.json()
+        ftoken = floginresponse['imdata'][0]['aaaLogin']['attributes']['token']
+        if fcontentype == 'xml':
+            fsession_api.headers.update({'APIC-cookie': ftoken,
+            'Content-Type': 'application/xml'})
+        else:
+            fsession_api.headers.update({'APIC-cookie': ftoken,
+            'Content-Type': 'application/json'})
+        return fsession_api
+
+
+def aci_auth_logout(fapicip, fapic_session):
+    fresponse = fapic_session.post(url= "https://"+fapicip+"/api/aaaLogout.json",
+    verify=False)
+    if fresponse.status_code != 200:
+        print(json.dumps(response.json(), indent = 4))
+    else:
+        print("Logout efetuado com sucesso")
+
+
+apicip, username, password = argv[1:]
+horariodata_atual = datetime.now()
+horariodata_formatada = horariodata_atual.strftime("%d-%m-%Y")
+apic_session = aci_auth_session(apicip, username, password)
+baseurl = "https://"+apicip+"/api/node/mo/"
+get_all_endpoints = "uni/.json?query-target=subtree&target-subtree-class=fvCEp&rsp-prop-include=naming-only"
+get_all_epgs = "uni/.json?query-target=subtree&target-subtree-class=fvAEPg&rsp-prop-include=naming-only"
+all_endpoints = apic_session.get(url = baseurl+get_all_endpoints, verify=False).json()
+all_epgs = apic_session.get(url = baseurl+get_all_epgs, verify=False).json()
+all_epgs_prod_2 = []
+all_dn_with_no_endpoints = []
+
+
+# Se dentro da excecao, pule o epg. Se nao tem endpoints coloque na lista all_dn_with_no_endpoints.
+for epg in all_epgs["imdata"]:
+    matchs_exception_list = False
+    for tn in exception_list:
+        if tn in epg["fvAEPg"]["attributes"]["dn"]:
+            matchs_exception_list = True
+    if matchs_exception_list == True:
+        continue
+    if epg["fvAEPg"]["attributes"]["name"] not in str(all_endpoints):
+#        print(f'epg {epg["fvAEPg"]["attributes"]["dn"]} esta vazio')
+        all_dn_with_no_endpoints.append(epg["fvAEPg"]["attributes"]["dn"])
+
+# Obs: O nome do epg k8s é o mesmo nos dois tenants k8s, tb e gt.
+# Com o if pelo nome do EPG, garantimos que somente quando uma aplicação k8s nao tiver endpoint nos DOIS sites os EPGs serao removidos.
+
+all_dn_with_no_eps_all_tenants = []
+all_dn_with_no_eps_k8s = []
+
+# Se nao tem endpoints em nenhum dos dois epgs k8s irmaos e tem K8S no path coloque na lista all_dn_with_no_eps_k8s.
+# Se nao tem endpoints, o que restou vai para all_dn_with_no_eps_all_tenants.
+for dn in all_dn_with_no_endpoints:
+    if "K8S" in dn:
+        dn = sub("\/epg\-.*", '', dn)
+        all_dn_with_no_eps_k8s.append(dn)
+    else:
+        all_dn_with_no_eps_all_tenants.append(dn)
+
+epgs_removidos = []
+ultima_consulta_str = ""
+
+# Abertura da ultima coleta para confronto.
+with open ('ultima_consulta.txt', 'r') as ultima_consulta:
+    for line in ultima_consulta:
+        ultima_consulta_str += line
+
+# Faz backup dos tenants
+if len(ultima_consulta_str) > 0:
+    faz_backup_ACI = {"configExportP":{"attributes":{"dn":"uni/fabric/configexp-defaultOneTime","name":"defaultOneTime",
+	"snapshot":"true","targetDn":"","adminSt":"triggered","rn":"configexp-defaultOneTime","status":"created,modified",
+	"descr":horariodata_formatada+'_remocao_de_epgs'},"children":[]}}
+    apic_session.post(url = baseurl+"uni/fabric/configexp-defaultOneTime.json",
+    data=json.dumps(faz_backup_ACI), verify=False).json
+
+# Se epg ou app esta dentro da coleta anterior, deleta o epg ou app e faz um backup com data.
+for dn in all_dn_with_no_eps_all_tenants:
+    if dn in ultima_consulta_str:
+        payload = {"fvAEPg":{"attributes":{"dn":dn,"status":"deleted"},
+        "children":[]}}
+        backup = apic_session.get(url = baseurl+dn+
+        ".json?query-target=self&rsp-subtree=full&rsp-prop-include=config-only",
+        verify=False).json()
+#        print(f"Efetuando backup do {dn}")
+        epg_name = findall('(?<=tn-).*', dn)
+        epg_name = sub('\/', '_', epg_name[0])
+        file_name = horariodata_formatada+"_"+epg_name+".json"
+        with open (file_name, 'w') as backup_epg:
+            backup_epg.write(json.dumps(backup, indent = 4))
+#        print(f"Deletando {dn}")
+        apic_session.post(url = baseurl+dn+".json", data=json.dumps(payload), verify=False)
+        epgs_removidos.append(dn)
+
+for dn in all_dn_with_no_eps_k8s:
+    if dn in ultima_consulta_str:
+        payload = {"fvAp":{"attributes":{"dn":dn,"status":"deleted"},
+        "children":[]}}
+        backup = apic_session.get(url = baseurl+dn+
+        ".json?query-target=self&rsp-subtree=full&rsp-prop-include=config-only",
+        verify=False).json()
+#        print(f"Efetuando backup do {dn}")
+        app_name = findall('(?<=tn-).*', dn)
+        app_name = sub('\/', '_', app_name[0])
+        file_name = horariodata_formatada+"_"+app_name+".json"
+        with open (file_name, 'w') as backup_app:
+            backup_app.write(json.dumps(backup, indent = 4))
+#        print(f"Deletando {dn}")
+        apic_session.post(url = baseurl+dn+".json", data=json.dumps(payload),
+        verify=False)
+        epgs_removidos.append(dn)
+
+# Depois de removidos os epgs ou apps removemos da lista mais atual essas entradas,
+# deixando somente novos epgs ou apps sem endpoints que serao confrontados
+# na proxima consulta.
+for epg_app in epgs_removidos:
+    try:
+        all_dn_with_no_eps_k8s.remove(epg_app)
+    except:
+        all_dn_with_no_eps_all_tenants.remove(epg_app)
+
+
+print ("#### ATENCAO: OS EPGS ABAIXO FORAM REMOVIDOS POIS ESTAVAM A 30 DIAS SEM ENDPOINT DESDE A ULTIMA CONSULTA ####")
+print(f"Total de EPGs removidos {len(epgs_removidos)}")
+
+for epg_app_removido in epgs_removidos:
+    print(epg_app_removido)
+
+print ("#### ATENCAO: OS EPGS ABAIXO SERAO REMOVIDOS DAQUI A 30 DIAS CASO NAO HAJA ENDPOINTS NA PROXIMA CONSULTA ####")
+print(f"Total de EPGs que serao removidos {len(all_dn_with_no_eps_k8s)+len(all_dn_with_no_eps_all_tenants)}")
+
+# Escrevemos no arquivo a lista atualizada para ser confrontada na proxima consulta.
+with open ('ultima_consulta.txt', 'w') as ultima_consulta:
+    for dn in all_dn_with_no_eps_k8s:
+        ultima_consulta.write(dn+"\n")
+        print(f'{dn}')
+    for dn in all_dn_with_no_eps_all_tenants:
+        ultima_consulta.write(dn+"\n")
+        print(f'{dn}')
+
+aci_auth_logout(apicip, apic_session)
---
+++ Automação ACI - Remove EPG ou APP sem endpoint/exception_list_DEV_QA.py
@@ -0,0 +1,2 @@
+exception_list_DEV_QA = ["tn-common", "tn-infra", "ap-kubernetes", "Interno"]
+
